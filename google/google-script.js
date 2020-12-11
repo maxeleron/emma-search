0c01805c386e9f988161232cfbaa3e04fd57094e
Max Ponomarenko <maxeleron@gmail.com>
2020-12-11 10:57:43 +0200

    0.1 version, added descritiption and main files

---
+++ google/google-script.js
@@ -0,0 +1,5 @@
+'use strict'
+
+alert('Hi Google script is working')
+//document.getElementsByClassName('.g').style.color = 'red'
+
---
+++ yandex/yandex-script.js
@@ -0,0 +1,5 @@
+'use strict'
+
+alert('Hi Yandex script is working')
+//document.getElementsByClassName('.g').style.color = 'red'
+
