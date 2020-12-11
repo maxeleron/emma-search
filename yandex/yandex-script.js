71e383140d8848573dd7c49c51db4d25c1f02036
Max Ponomarenko <maxeleron@gmail.com>
2020-12-11 12:49:08 +0200

    started work at yandex - exploring how to move, resize and hide elements on search page

--- yandex/yandex-script.js
+++ yandex/yandex-script.js
@@ -1,5 +1,11 @@
 'use strict'
 
-alert('Hi Yandex script is working')
-//document.getElementsByClassName('.g').style.color = 'red'
+//alert('Hi Yandex script is working')
 
+//1-link open by Enter feature
+window.addEventListener('keyup', (e)=>{
+    if (e.keyCode === 13) {
+        //clicks on the first link when Enter hited
+        document.querySelector("#search-result > li:nth-child(3) > div.organic.typo.typo_text_m.typo_line_s.i-bem > h2 > a").click()
+      }
+})
