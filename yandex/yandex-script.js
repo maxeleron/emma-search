d1937abe367800e921c587dbf9cd38075af2f5e3
Max Ponomarenko <maxeleron@gmail.com>
2020-12-15 11:55:55 +0200

    added setting fields in popup, different icons resolutions, tested yandex search page

--- yandex/yandex-script.js
+++ yandex/yandex-script.js
@@ -4,7 +4,8 @@
 
 //1-link open by Enter feature
 window.addEventListener('keyup', (e)=>{
-    if (e.keyCode === 13) {
+    //let searchInput = document.querySelector('.input__control');
+    if (e.keyCode === 13 && true) {// there should be smart enter reading in future
         //clicks on the first link when Enter hited
         document.querySelector("#search-result > li:nth-child(3) > div.organic.typo.typo_text_m.typo_line_s.i-bem > h2 > a").click()
       }
